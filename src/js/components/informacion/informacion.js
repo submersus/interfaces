d7a4b92dbc97b07ac84d76b3d7808b1ff1965cda
Alirio A. Angel <alirioangel.1993@gmail.com>
2018-06-05 15:45:27 -0400

    asd

---
+++ src/js/components/informacion/informacion.js
@@ -0,0 +1,11 @@
+import React , {Component} from 'react';
+
+class Informacion extends Component {
+    render = ()=>{
+        <div>
+            <h1>Bienvenido a la seccion de informacion</h1>
+        </div>
+    }
+}
+
+export default Informacion;
